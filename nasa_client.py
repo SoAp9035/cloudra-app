070fbe6aa393b0d4b53cbc3910677b14592b0afa
SoAp9035 <ahmetburhan1703@gmail.com>
2025-09-22 16:06:28 +0300

    Organize the file

--- nasa_client.py
+++ nasa_client.py
@@ -48,8 +48,8 @@ class NASAPowerAPI:
 
     def get_historical_data(
         self,
-        lon: float,
         lat: float,
+        lon: float,
         start_date: str,
         end_date: str,
         parameters: list[str] | dict,
@@ -63,8 +63,8 @@ class NASAPowerAPI:
         params = {
             "parameters": ",".join(parameters),
             "community": "AG",
-            "longitude": lon,
             "latitude": lat,
+            "longitude": lon,
             "start": start_date,
             "end": end_date,
             "format": format,
@@ -72,92 +72,11 @@ class NASAPowerAPI:
 
         response = requests.get(self.BASE_URL, params=params)
         return response.json() if format == "JSON" else response.text
-    
-    ### Konuma ait TÜM verileri çekecek fonksiyonlar ###
-    # Geliştirme aşamasında #
-
-    # def get_historical_data_by_year(
-    #     self,
-    #     lon: float,
-    #     lat: float,
-    #     year: int,
-    #     parameters: list[str] | dict,
-    #     format: str = "JSON",
-    # ) -> dict | str:
-    #     """
-    #     NASA POWER API'den girilen yılın tüm günlerinin tarihi verisini çeker.
-    #     """
-    #     params = {
-    #         "parameters": ",".join(parameters),
-    #         "community": "AG",
-    #         "longitude": lon,
-    #         "latitude": lat,
-    #         "start": f"{year}0101",
-    #         "end": f"{year}1230",
-    #         "format": format,
-    #     }
-
-    #     response = requests.get(self.BASE_URL, params=params)
-    #     return response.json() if format == "JSON" else response.text
-
-    # def get_multi_year_data(
-    #     self,
-    #     lon: float,
-    #     lat: float,
-    #     years_back: int,
-    #     parameters: list[str] | dict = POWER_PARAMETERS,
-    #     format: str = "JSON",
-    # ) -> StringIO | None:
-    #     """
-    #     Şimdiki yılından önceki son X yılının tüm verilerini çeker.
-    #     """
-    #     all_data = []
-    #     current_year = datetime.now().year
-        
-    #     for year in range(current_year - years_back, current_year):
-    #         try:
-    #             data = self.get_historical_data_by_year(
-    #                 lon=lon,
-    #                 lat=lat,
-    #                 year=current_year,
-    #                 parameters=parameters,
-    #                 format="JSON"
-    #             )
-    #             all_data.append(data["properties"]["parameter"])
-    #         except Exception as e:
-    #             print(f"Something went wrong with NASAPowerAPI/get_multi_year_data. Error: {e}")
-    #             continue
-
-    #         time.sleep(.1) # API'den banlanmamak için gecikme
-
-    #     if not all_data:
-    #         print("get_multi_year_data: No data returned by API.")
-    #         return None
-
-    #     try: 
-    #         df = pd.concat([pd.DataFrame(item) for item in all_data])
-
-    #         df.index = pd.to_datetime(df.index)
-    #         df.sort_index(inplace=True)
-    #         df.index.name = "DATE"
-
-    #         if format == "JSON":
-    #             df_copy = df.copy()
-    #             df_copy.index = df_copy.index.strftime("%Y-%m-%d")
-    #             return StringIO(df_copy.to_json())
-    #         else:
-    #             return StringIO(df.to_csv())
-
-    #         # Eski yöntem: str olarak döndürür
-    #         # return df.to_json() if format == "JSON" else df.to_csv()
-    #     except Exception as e:
-    #         print(f"Something went wrong with Pandas/get_multi_year_data. Error: {e}")
-    #         return None
 
     def get_multi_year_data_for_day(
         self,
-        lon: float,
         lat: float,
+        lon: float,
         month: int,
         day: int,
         day_range: int,
@@ -167,6 +86,9 @@ class NASAPowerAPI:
     ) -> StringIO | None:
         """
         Belirli bir gün için son X yılın verilerini çeker.
+
+        Returns:
+            Pandas tarafından okunabilir StringIO.
         """
         all_data = []
         current_year = datetime.now().year
@@ -178,8 +100,8 @@ class NASAPowerAPI:
                 end = date + timedelta(days=day_range)
                 
                 data = self.get_historical_data(
-                    lon=lon,
                     lat=lat,
+                    lon=lon,
                     start_date=start.strftime("%Y%m%d"),
                     end_date=end.strftime("%Y%m%d"),
                     parameters=parameters,
