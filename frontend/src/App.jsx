8768a24ff05c4364582b71b361ccc2f07aa6ea7f
SoAp9035 <ahmetburhan1703@gmail.com>
2025-10-05 08:02:57 +0300

    Fix sidebar view issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -89,6 +89,7 @@ export default function App() {
 
     setLoading(true);
     setError("");
+    setHasSelectedLocation(true);
     try {
       const json = await fetchWeatherProbability({
         lat: markerPosition.lat,
